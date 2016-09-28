14cebd53a4e34d1b227bbd6366a2f22a09612b06
bkinsella1 <787866@seq.org>
2016-09-28 08:31:56 -0700

    Add files via upload

---
+++ Project8 - ReversingArray/ReverseArray.java
@@ -0,0 +1,13 @@
+
+/**
+ *Brett Kinsella
+ *9/21/16
+ *Reverse Array
+ */
+public class ReverseArray
+{
+    int[] nums = new int[10];
+    public int[] reverseArray (int[]x){
+        return x;
+    }
+}
---
+++ Project8 - ReversingArray/ReverseArrayRunner.java
@@ -0,0 +1,11 @@
+
+/**
+ * Brett Kinsella
+ * 9/21/16
+ * Reverse Array Runner
+ */
+public class ReverseArrayRunner
+{
+    public static void main (String[] args){
+    }
+}

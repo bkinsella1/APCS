a3bcd966275c77fae8672d2de12af4fcd01d8de1
bkinsella1 <787866@seq.org>
2016-09-28 08:31:11 -0700

    Add files via upload

---
+++ Project 2 - Ball World/BallRunner.java
@@ -0,0 +1,19 @@
+
+/**
+ * Brett Kinsella
+ * 9/1/2016
+ * BallRunner
+ */
+public class BallRunner
+{
+    public void run(){
+        BallWorld map = new BallWorld (200,200);
+        TGPoint point = new TGPoint (0,0);
+        BallBot bot1 = new BallBot (map,point,0,10);
+        BallBot[] b1 = new BallBot[10];
+        //for (int num = 0; num < 0; num ++)
+        //TGPoint = new TGPoint
+        
+        
+    }
+}
